82b0a3c1c0be13c35afba5282e5da38a5b950cb6
Jean <jean.carlosmolossi@gmail.com>
2022-06-05 04:57:34 -0300

    refactor: simplificada rota com controller

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
+const { transactionController } = require('./controller/transaction')
 const { createServer } = require('./server')
 
 const server = createServer()
 server.get('/ping', (req, res) => {
-    return res.end(JSON.stringify({ message: 'Pong!' }))
+    return res.status(200).json({ message: 'Pong!' })
 })
 
-server.post('/transaction', (req, res) => {
-    return res.end(JSON.stringify(req.body || null))
-})
-
-server.delete('/transaction/:id', (req, res) => {
-    return res.end(JSON.stringify({...req.params, 'what': 'happened'}))
-})
-
+server.post('/transaction', transactionController.create)
+server.delete('/transaction/:id', transactionController.remove)
 server.get('/transactions', (req, res) => {
     return res.end(JSON.stringify({ transactions: 'ok' }))
 })
-
 const PORT = process.env.PORT || 8080
 
 server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))

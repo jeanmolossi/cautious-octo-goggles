a32c0f475dd75141b91edb303644b0df12491ca3
Jean <jean.carlosmolossi@gmail.com>
2022-06-05 22:25:35 -0300

    feat: criada rota de historico

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ const { transactionController } = require('./controller/transaction')
 const { createServer } = require('./server')
 
 const server = createServer()
-server.get('/ping', (req, res) => {
-    return res.status(200).json({ message: 'Pong!' })
+server.get('/ping', (_, res) => {
+    return res.json({ message: 'Pong!' })
 })
 
 server.post('/transaction', transactionController.create)
 server.delete('/transaction/:id', transactionController.remove)
-server.get('/transactions', (req, res) => {
-    return res.end(JSON.stringify({ transactions: 'ok' }))
-})
+server.get('/transactions', transactionController.history)
 const PORT = process.env.PORT || 8080
 
 server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))

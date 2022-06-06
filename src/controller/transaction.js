366aa3633395080361af62336e9caf112b603a10
Jean <jean.carlosmolossi@gmail.com>
2022-06-05 22:42:54 -0300

    feat: utilizando services

--- src/controller/transaction.js
+++ src/controller/transaction.js
@@ -1,5 +1,8 @@
 const http = require('http')
 const { Transaction } = require('../models/transaction')
+const createTransaction = require('../service/create-transaction')
+const removeTransaction = require('../service/remove-transaction')
+const transactionHistory = require('../service/transaction-history')
 
 var transactionController = {}
 
@@ -9,9 +12,10 @@ var transactionController = {}
  * @param {http.ServerResponse} response 
  */
 transactionController.create = function create(request, response) {    
-    const transaction = new Transaction(request.body)
+    const transaction = new Transaction(request.body)    
+    const savedTransaction = createTransaction(transaction)
     
-    return response.status(201).json(transaction)
+    return response.status(201).json(savedTransaction)
 }
 
 transactionController.remove = function remove(request, response) {
@@ -23,11 +27,21 @@ transactionController.remove = function remove(request, response) {
             .json({ error: 'id is not set' })
     }
     
-    return response.status(204).json()
+    const deleted = removeTransaction(params.id)
+    if (deleted)
+        return response.status(204).json()
+    
+    return response.status(404).json({ error: 'transaction not found' })
 }
 
 transactionController.history = function history(request, response) {
-    return response.json([])
+    const transactions = transactionHistory()
+    
+    if (transactions.length === 0) {
+        return response.status(204).json()
+    }
+    
+    return response.json(transactions)
 }
 
 module.exports = { transactionController }
